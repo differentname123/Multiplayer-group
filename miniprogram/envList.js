fa8dd305cdd500afe2aec772409bb688dad9401f
differentname123 <2434954452@qq.com>
2024-10-03 22:32:31 +0800

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"demo-8g342rg41cedb115","alias":"demo"}]
+const isMac = false
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.bat
@@ -0,0 +1,1 @@
+"D:\install\微信web开发者工具\cli.bat" cloud functions deploy --e demo-8g342rg41cedb115 --n quickstartFunctions --r --project "W:\project\we_project\miniprogram-1" --report_first --report
